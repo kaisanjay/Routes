eea7f7f6cd4048952354cfa9cbe58ddc22dd7d38
kaisanjay <sanjaykai.1994@gmail.com>
2018-06-04 04:20:56 +0530

    switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter , Route } from "react-router-dom";
+import { BrowserRouter , Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -12,14 +12,14 @@ class App extends Component {
        that we can wrap all our route inside our application this BrowserRouter acts a wrapper div */
 
       <BrowserRouter > 
-        <div>      
+        <Switch>      
         
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
           <Route component={Error}/>
 
-        </div>
+        </Switch>
       </BrowserRouter>
     );
   }
