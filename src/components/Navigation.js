345f7a333d2fa93125a32fa050c1ed4f2e480651
kaisanjay <sanjaykai.1994@gmail.com>
2018-06-04 04:51:22 +0530

    NavLink

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
     return (
         <div>
-            <p> Navigation</p>
+           <NavLink to="/">Home</NavLink>
+           <NavLink to="/about">About</NavLink>
+           <NavLink to="/contact">Contact</NavLink>
         </div>
     );
 };
